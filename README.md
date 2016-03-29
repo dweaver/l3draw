1596d2a46358ecd19bb443a53597bb31552708b3
Dan Weaver <lastfinger@gmail.com>
2016-03-28 23:32:29 -0500

    add L3D drawing canvas w/three.js

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Draw things on an [L3D Cube](http://lookingglassfactory.com/).
 $ npm install
 $ npm run dev
 ```
+
+![L3D Drawing Canvas Screenshot](/images/l3draw.png "L3D Drawing Canvas")
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,10 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const THREE = require('three');
 
 const helpers = require('../utils/helpers');
 
-class Main extends React.Component {
+export default class Main extends React.Component {
   constructor() {
     super();
     this.setVoxel = this.setVoxel.bind(this);
@@ -46,5 +47,93 @@ class Main extends React.Component {
     );
   }
 }
+
+// Set up three.js
+function cubeSetup() {
+  const scene = new THREE.Scene();
+  const cubeElement = document.getElementById('cube');
+  const innerWidth = 600;
+  const innerHeight = 600;
+  const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+  const unselectedColor = 0x666666;
+  const selectedColor = 0xffff44;
+  camera.position.z = 5;
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(innerWidth, innerHeight);
+  cubeElement.appendChild(renderer.domElement);
+
+  const geometry = new THREE.SphereGeometry(0.1);
+  //const material = new THREE.MeshBasicMaterial(
+  //  { color: 0x70d030, wireframe: true, transparent: true });
+  const cubeOfCubes = new THREE.Group();
+
+  var light = new THREE.DirectionalLight( 0xffffff, 1 );
+  light.position.set( 1, 1, 1 ).normalize();
+  scene.add( light );
+
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      for (let k = 0; k < 8; k++) {
+        const cube = new THREE.Mesh(
+          geometry,
+          new THREE.MeshLambertMaterial( { color: unselectedColor } ));
+        cube.position.set((-3.5 + i) * 0.5, (-3.5 + j) * 0.5, (-3.5 + k) * 0.5);
+        cube.l3dCoord = [i, j, k];
+        cubeOfCubes.add(cube);
+      }
+    }
+  }
+
+  scene.add(cubeOfCubes);
+
+  const mouse = new THREE.Vector2();
+  let INTERSECTED;
+  const raycaster = new THREE.Raycaster();
+  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+  document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+    
+  function onDocumentMouseMove( event ) {
+    event.preventDefault();
+    // TODO: account for scrolling
+    mouse.x = ( event.clientX / innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / innerHeight ) * 2 + 1;
+  }
+
+  function onDocumentMouseUp( event ) {
+    event.preventDefault();
+    if (INTERSECTED !== null) {
+      console.log('clicked!', INTERSECTED.l3dCoord);
+      INTERSECTED.material.color.setHex( selectedColor );
+    }
+  }
+
+  function render() {
+    requestAnimationFrame(render);
+    // cubeOfCubes.rotation.x += 0.001;
+    // cubeOfCubes.rotation.y += 0.001;
+    // cubeOfCubes.rotation.z += 0.001;
+    
+    // find intersections
+    raycaster.setFromCamera( mouse, camera );
+    var intersects = raycaster.intersectObjects( cubeOfCubes.children );
+    if ( intersects.length > 0 ) {
+      if ( INTERSECTED != intersects[ 0 ].object ) {
+        if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+        INTERSECTED = intersects[ 0 ].object;
+        INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+        INTERSECTED.material.emissive.setHex( 0xff0000 );
+      }
+    } else {
+      if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+      INTERSECTED = null;
+    }
+
+    renderer.render(scene, camera);
+  }
+  render();
+}
+cubeSetup();
+
 
 ReactDOM.render(<Main />, document.getElementById('app'));
